3ca0c7c66c1ea7651de6ef85f7c5b7432ddfa564
William <william.jsson@hotmail.com>
2024-03-28 11:04:38 +0100

    womp womp

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const Morgan = require('morgan');
 const BodyParser = require('body-parser');
 const stream = require('stream');
 const fs = require('fs');
+const session = require('express-session');
 const { Writable } = stream;
-const { insert_new_blogpost } = require('./private/db_func.js');
+const { insert_new_blogpost,
+    create_new_user,
+    login_user,
+} = require('./private/db_func.js');
 const { blog_post } = require('./private/blogpost.js');
 
 const app = Express();
@@ -16,6 +20,16 @@ app.use(Morgan('dev'));
 app.set('view engine', 'ejs');
 app.use(Express.static('public'));
 app.use(Express.json());
+
+// Create a session 
+app.use(session
+    ({
+        secret: 'secret',
+        resave: true,
+        saveUninitialized: true,
+        username: ''
+    }));
+
 
 // Create a write stream (append mode) to the log file
 if (!fs.existsSync('./logs')) {
@@ -35,6 +49,45 @@ app.get('/', (req, res) => {
 app.get("/writeups", (req, res) => {
     res.render("writeups.ejs");
 });
+
+app.get("/login", (req, res) => {
+    const query = req.query;
+    const username = query.user;
+    const password = query.password;
+    login_user(username, password, callback => {
+        if (callback) {
+            res.send("User logged in successfully");
+            console.log(`User ${username} logged in successfully`);
+        } else {
+            res.send("User not found");
+            console.log(`User ${username} not found`);
+        }
+    });
+    // Set express session
+    req.session.username = username;
+    res.render("index.ejs",
+        {
+            username: req.session.username
+        });
+});
+
+// Create a new user from the terminal interface
+app.get("/createnewuser", (req, res) => {
+    // Fetch the query parameters
+    const query = req.query;
+    const username = query.user;
+    const password = query.password;
+    create_new_user(username, password, callback => {
+        if (callback) {
+            res.send("User created successfully");
+            console.log(`User ${username} created successfully`);
+        } else {
+            res.send("User already exists");
+            console.log(`User ${username} attempted to be created but already exists`);
+        }
+    });
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
--- private/db_func.js
+++ private/db_func.js
@@ -1,9 +1,10 @@
 const sqlite3 = require('sqlite3');
+const bcrypt = require('bcrypt');
 const { blog_post } = require('./blogpost.js');
 
 // If object of db is not created, create new class of db
 class db {
-    constructor(){
+    constructor() {
         this.db = db_validation();
     }
 }
@@ -11,7 +12,7 @@ class db {
 db_conn = new db();
 
 // Create new db if it doesnt exist
-function db_validation(){
+function db_validation() {
     db = new sqlite3.Database('blog.sqlite', (err) => {
         if (err) {
             console.error(err.message);
@@ -19,13 +20,14 @@ function db_validation(){
         console.log('Connected to the database.');
         db.serialize(() => {
             db.run('CREATE TABLE IF NOT EXISTS blog (id INTEGER PRIMARY KEY, title TEXT, content TEXT)');
+            db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)');
         });
     });
     return db;
 }
 
-function insert_new_blogpost(blog_post){
-    if(fetch_post_by_name(blog_post.title)){
+function insert_new_blogpost(blog_post) {
+    if (fetch_post_by_name(blog_post.title)) {
         console.log("Post with this title already exists");
         return;
     }
@@ -34,7 +36,7 @@ function insert_new_blogpost(blog_post){
     });
 }
 
-function fetch_post_by_name(title){
+function fetch_post_by_name(title) {
     db_conn.db.serialize(() => {
         db_conn.db.get('SELECT * FROM blog WHERE title = ?', [title], (err, row) => {
             if (err) {
@@ -45,6 +47,52 @@ function fetch_post_by_name(title){
     });
 }
 
+function login_user(username, password, callback) {
+    // Attempt to login a user with the given username and password
+    db_conn.db.serialize(() => {
+        db_conn.db.get('SELECT * FROM users WHERE username = ?', [username], (err, row) => {
+            if (err) {
+                console.error(err.message);
+                callback(false); // Call the callback with false if an error occurred
+            } else {
+                if (row) {
+                    // Check if the password is correct
+                    if (bcrypt.compareSync(password, row.password)) {
+                        callback(true); // Call the callback with true if the password is correct
+                    } else {
+                        callback(false); // Call the callback with false if the password is incorrect
+                    }
+                } else {
+                    callback(false); // Call the callback with false if the user doesn't exist
+                }
+            }
+        });
+    });
+}
+
+function create_new_user(username, password, callback) {
+    // Attempt to create a new user in db if it doesn't exist
+
+    // Hash password with bcrypt
+    const saltRounds = 10;
+    const salt = bcrypt.genSaltSync(saltRounds);
+    password = bcrypt.hashSync(password, salt);
+
+    // Insert the new user into the db
+    db_conn.db.serialize(() => {
+        db_conn.db.run('INSERT INTO users (username, password) VALUES (?, ?)', [username, password], (err) => {
+            if (err) {
+                console.error(err.message);
+                callback(false); // Call the callback with false if an error occurred
+            } else {
+                callback(true); // Call the callback with true if the operation was successful
+            }
+        });
+    });
+}
+
 module.exports = {
-    insert_new_blogpost
+    insert_new_blogpost,
+    create_new_user,
+    login_user,
 }
