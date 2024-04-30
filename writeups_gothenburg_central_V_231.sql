9f7eed5a6d1d50386eaa6164c2c425df7e287c66
William <william.jsson@hotmail.com>
2024-04-30 09:05:38 +0200

    Add readme.md & dummy database.

--- writeups_gothenburg_central_V_231.sql
+++ writeups_gothenburg_central_V_231.sql
@@ -1,10 +1,3 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               8.0.30 - MySQL Community Server - GPL
--- Server OS:                    Win64
--- HeidiSQL Version:             12.1.0.6537
--- --------------------------------------------------------
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
